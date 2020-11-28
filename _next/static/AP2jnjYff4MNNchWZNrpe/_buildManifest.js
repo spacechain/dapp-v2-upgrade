b7a35cd5bff8c38af57c06bb7cbf72b98399b51d
gabmontes <gabmontes@users.noreply.github.com>
2020-11-28 13:23:03 +0000

    deploy: 532f978d62a3e8beb2de5fd2b325297dfe954a93

---
+++ _next/static/AP2jnjYff4MNNchWZNrpe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0b0ee6a7b22f3c53e7ec.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a5065f039093f233cec7.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
